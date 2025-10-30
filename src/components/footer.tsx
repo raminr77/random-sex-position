a07705251a2d7202e00512c4b20ebe9ec85f0d83
Ramin <Ramin.rezaei77@gmail.com>
2025-10-30 20:17:56 +0100

    fix: style issues

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="w-full text-sm text-slate-400 mb-4 flex items-center justify-center gap-1 fixed bottom-5">
+    <footer className="w-full text-sm text-slate-400 mb-4 flex items-center justify-center gap-1 mt-10 mb-5">
       <span>Made with love by</span>
       <a
         target="_blank"
